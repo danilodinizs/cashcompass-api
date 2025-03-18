acc129eada96e4b9f7da31ad03e14f851c92662f
Danilo Diniz Silva <danilo.silva@walsywa.local>
2025-03-18 17:16:38 -0300

    Initial commit

---
+++ src/test/java/dev/danilodinizs/cashcompass_api/CashcompassApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danilodinizs.cashcompass_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcompassApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
