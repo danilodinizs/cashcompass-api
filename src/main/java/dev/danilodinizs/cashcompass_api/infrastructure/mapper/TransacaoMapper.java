796803da64c2e942215a781784a07d9f0d5bf09b
danilodinizs <danilodiniz93@yahoo.com>
2025-03-19 16:24:50 -0300

    feat: create TransacaoMapper

---
+++ src/main/java/dev/danilodinizs/cashcompass_api/infrastructure/mapper/TransacaoMapper.java
@@ -0,0 +1,62 @@
+package dev.danilodinizs.cashcompass_api.infrastructure.mapper;
+
+import dev.danilodinizs.cashcompass_api.core.entities.Transacao;
+import dev.danilodinizs.cashcompass_api.infrastructure.dtos.TransacaoRequestDTO;
+import dev.danilodinizs.cashcompass_api.infrastructure.persistence.TransacaoEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransacaoMapper {
+
+    public TransacaoRequestDTO domainToDto(Transacao transacao) {
+        return new TransacaoRequestDTO(
+                transacao.id(),
+                transacao.descricao(),
+                transacao.categoria(),
+                transacao.data(),
+                transacao.valor(),
+                transacao.tipoTransacao(),
+                transacao.parcela(),
+                transacao.nomeDevedor()
+        );
+    }
+
+    public Transacao dtoToDomain(TransacaoRequestDTO transacaoDTO) {
+        return new Transacao(
+                transacaoDTO.id(),
+                transacaoDTO.descricao(),
+                transacaoDTO.categoria(),
+                transacaoDTO.data(),
+                transacaoDTO.valor(),
+                transacaoDTO.tipoTransacao(),
+                transacaoDTO.parcela(),
+                transacaoDTO.nomeDevedor()
+        );
+    }
+
+    public TransacaoEntity domainToEntity(Transacao transacao) {
+        return new TransacaoEntity(
+                transacao.id(),
+                transacao.descricao(),
+                transacao.categoria(),
+                transacao.data(),
+                transacao.valor(),
+                transacao.tipoTransacao(),
+                transacao.parcela(),
+                transacao.nomeDevedor()
+        );
+    }
+
+    public Transacao entityToDomain(TransacaoEntity transacaoEntity) {
+        return new Transacao(
+                transacaoEntity.getId(),
+                transacaoEntity.getDescricao(),
+                transacaoEntity.getCategoria(),
+                transacaoEntity.getData(),
+                transacaoEntity.getValor(),
+                transacaoEntity.getTipoTransacao(),
+                transacaoEntity.getParcela(),
+                transacaoEntity.getNomeDevedor()
+        );
+    }
+}
