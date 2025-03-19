0b5e0ea3294a20b18cea2109a7fa8cf1f4692243
danilodinizs <danilodiniz93@yahoo.com>
2025-03-19 16:49:20 -0300

    feat: update Json LocalDateTime to now

--- src/main/java/dev/danilodinizs/cashcompass_api/infrastructure/dtos/TransacaoRequestDTO.java
+++ src/main/java/dev/danilodinizs/cashcompass_api/infrastructure/dtos/TransacaoRequestDTO.java
@@ -1,5 +1,8 @@
 package dev.danilodinizs.cashcompass_api.infrastructure.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import dev.danilodinizs.cashcompass_api.core.enums.TipoTransacao;
 
 import java.math.BigDecimal;
@@ -9,9 +12,18 @@ import java.util.UUID;
 public record TransacaoRequestDTO(UUID id,
                                   String descricao,
                                   String categoria,
+
+                                  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy'T'HH:mm")
+                                  @JsonSerialize(using = LocalDateTimeSerializer.class)
                                   LocalDateTime data,
                                   BigDecimal valor,
                                   TipoTransacao tipoTransacao,
                                   String parcela,
-                                  String nomeDevedor) {
+                                  String nomeDevedor)
+{
+    public TransacaoRequestDTO {
+    if (data == null) {
+        data = LocalDateTime.now();
+        }
+    }
 }
