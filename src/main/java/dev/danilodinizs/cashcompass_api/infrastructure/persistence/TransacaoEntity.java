d504361b8b398665ec30f04a1bd740b05c935e9f
danilodinizs <danilodiniz93@yahoo.com>
2025-03-19 17:19:38 -0300

    feat: adds get and constructor methods

--- src/main/java/dev/danilodinizs/cashcompass_api/infrastructure/persistence/TransacaoEntity.java
+++ src/main/java/dev/danilodinizs/cashcompass_api/infrastructure/persistence/TransacaoEntity.java
@@ -39,4 +39,47 @@ public class TransacaoEntity {
 
     @NotNull
     private String nomeDevedor;
+
+    public TransacaoEntity(UUID id, String descricao, String categoria, LocalDateTime data, BigDecimal valor, TipoTransacao tipoTransacao, String parcela, String nomeDevedor) {
+        this.id = id;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.data = data;
+        this.valor = valor;
+        this.tipoTransacao = tipoTransacao;
+        this.parcela = parcela;
+        this.nomeDevedor = nomeDevedor;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public @NotNull String getDescricao() {
+        return descricao;
+    }
+
+    public @NotNull LocalDateTime getData() {
+        return data;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public @NotNull BigDecimal getValor() {
+        return valor;
+    }
+
+    public @NotNull TipoTransacao getTipoTransacao() {
+        return tipoTransacao;
+    }
+
+    public String getParcela() {
+        return parcela;
+    }
+
+    public @NotNull String getNomeDevedor() {
+        return nomeDevedor;
+    }
 }
