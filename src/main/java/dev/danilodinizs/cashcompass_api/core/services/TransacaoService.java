76b596dabc62f7812f96509d9d563fadcc616123
danilodinizs <danilodiniz93@yahoo.com>
2025-03-19 16:06:53 -0300

    feat: creates Service Class

---
+++ src/main/java/dev/danilodinizs/cashcompass_api/core/services/TransacaoService.java
@@ -0,0 +1,16 @@
+package dev.danilodinizs.cashcompass_api.core.services;
+
+import dev.danilodinizs.cashcompass_api.infrastructure.dtos.TransacaoRequestDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TransacaoService {
+
+    private static final Logger log = LoggerFactory.getLogger(TransacaoService.class)
+
+}
